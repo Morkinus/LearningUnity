c9bb2407bb6403d41cd1fcf56f463bcb5f1c2645
Morkinus <vygintasandr@gmail.com>
2016-02-23 21:54:37 +0200

    Beginning of Chapter 4
    Introduction to whiteboxing and material creation

---
+++ UnityInAction/Chapter4/Whiteboxing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public enum RotationAxes
+	{
+		MouseX = 0,
+		MouseY = 1
+	}
+
+	public RotationAxes axes = RotationAxes.MouseX;
+	public float rotationSensitivity = 5.0f;
+
+	public float vertMin = -50.0f;
+	public float vertMax = 50.0f;
+
+	private float vertRotation = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			transform.Rotate(0, Input.GetAxis ("Mouse X") * rotationSensitivity,0);
+		}
+		else if(axes == RotationAxes.MouseY)
+		{
+			vertRotation -= Input.GetAxis("Mouse Y")*rotationSensitivity;
+			vertRotation = Mathf.Clamp(vertRotation,vertMin,vertMax);
+
+			float horRotation = transform.localEulerAngles.y;
+
+			transform.localEulerAngles = new Vector3(vertRotation,horRotation,0);
+		}
+	}
+}
---
+++ UnityInAction/Chapter4/Whiteboxing/Assets/Scripts/Walking.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walking : MonoBehaviour {
+
+	public float speed = 7.0f;
+	public float gravity = -9.8f;
+	// Use this for initialization
+
+	private CharacterController ctrl;
+	void Start () {
+		ctrl = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed ;
+		float deltaZ = Input.GetAxis ("Vertical") * speed ;
+		Vector3 move = new Vector3 (deltaX, 0, deltaZ);
+		move = Vector3.ClampMagnitude (move, speed);
+
+		move.y = gravity;
+
+		move *= Time.deltaTime;
+		move = transform.TransformDirection (move);
+		ctrl.Move (move);
+	}
+}
