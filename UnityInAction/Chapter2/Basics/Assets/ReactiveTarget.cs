6e0dc58142bacd57aa1fdaf13a8605b836ae2bb0
Morkinus <vygintasandr@gmail.com>
2016-02-23 18:12:25 +0200

    End of Chapter 3
    Created enemy projectile

--- UnityInAction/Chapter2/Basics/Assets/ReactiveTarget.cs
+++ UnityInAction/Chapter2/Basics/Assets/ReactiveTarget.cs
@@ -5,6 +5,10 @@ public class ReactiveTarget : MonoBehaviour {
 
 	public void ReactToHit()
 	{
+		WanderingAI behavior = GetComponent<WanderingAI> ();
+		if (behavior != null) {
+			behavior.SetAlive(false);
+		}
 		StartCoroutine (Die ());
 	}
 
