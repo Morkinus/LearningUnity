a27fb725fe32c8807640e7bb7a231f230556d0aa
Morkinus <vygintasandr@gmail.com>
2016-02-29 20:44:12 +0200

    Created UI for FPS game
    Learned basics of creating UI for games. Used variuos event to call
    certains functions, performing actions in UI. Also used Messenger system
    from Unify

---
+++ UnityInAction/Chapter6/Basics/Assets/Scripts/SceneController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	[SerializeField]private GameObject enemyPrefab;
+	private GameObject _enemy;
+
+	void Awake()
+	{
+		Messenger<float>.AddListener (GameEvent.SPEED_CHANGED, speedChanged);
+	}
+
+	void OnDestroy()
+	{
+		Messenger<float>.RemoveListener (GameEvent.SPEED_CHANGED, speedChanged);
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (_enemy == null) {
+			_enemy = Instantiate (enemyPrefab) as GameObject;
+			_enemy.transform.position = new Vector3 (-4, 1, 4);
+			float angle = Random.Range (0, 360);
+			_enemy.transform.Rotate (0, angle, 0);
+
+		}
+	}
+
+	private void speedChanged(float value)
+	{
+		PlayerPrefs.SetFloat ("enemySpeed", value);
+	}
+}
