871e59ca54acf5186c27473071ba1b4ba1d5f359
Morkinus <vygintasandr@gmail.com>
2016-03-07 17:35:30 +0200

    Interactive devices
    Examples of creating objects player can interact with, setting up
    colelctibles, trigger areas, basic UI to show items collected, equipping
    and consuming collected items

---
+++ UnityInAction/Chapter8/Basics/Assets/Scripts/DeviceTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceTrigger : MonoBehaviour {
+
+	[SerializeField]private GameObject[] targets;
+
+	public bool RequireKey;
+
+	void OnTriggerEnter(Collider other)
+	{
+		//This was meant to be key item, but the collectible was named Stamina already, so i went with that
+		if (RequireKey && Managers.Inventory.equippedItem != "Stamina") {
+			return;
+		}
+
+		foreach (GameObject target in targets) {
+			target.SendMessage("Activate");
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		foreach (GameObject target in targets) {
+			target.SendMessage("Deactivate");
+		}
+	}
+}
