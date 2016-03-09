17d475e4aa511c0af0673a12b8ad1ecd9e8dc1a4
Morkinus <vygintasandr@gmail.com>
2016-03-09 17:24:18 +0200

    Networking
    Not finished...

---
+++ UnityInAction/Chapter9/Basics/Assets/Scripts/ImagesManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ImagesManager : MonoBehaviour, IGameManager {
+
+	public ManagerStatus status { get; private set; }
+
+	private NetworkService _network;
+
+	private Texture2D _webImage;
+
+	public void Startup(NetworkService service)
+	{
+		Debug.Log ("Images manager starting...");
+
+		_network = service;
+
+		status = ManagerStatus.Started;
+	}
+
+	public void GetWebImage(Action<Texture2D> callback)
+	{
+		if (_webImage == null) {
+			StartCoroutine (_network.DownloadImage (callback));
+		} else {
+			callback(_webImage);
+		}
+	}
+}
---
+++ UnityInAction/Chapter9/Basics/Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeatherManager : MonoBehaviour, IGameManager {
+
+	public ManagerStatus status { get; private set; }
+
+	//Add cloud value here, listing 9.8
+
+	private NetworkService _network;
+
+	public void Startup(NetworkService service)
+	{
+		Debug.Log ("Weather manager starting...");
+		_network = service;
+		StartCoroutine (_network.GetWeatherXML (OnXMLDataLoaded));
+		status = ManagerStatus.Initializing;
+	}
+
+	public void OnXMLDataLoaded(string data)
+	{
+		Debug.Log (data);
+		status = ManagerStatus.Started;
+	}
+}
