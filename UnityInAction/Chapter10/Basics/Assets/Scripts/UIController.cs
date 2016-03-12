a08b0e2b9c26cda5ae8297282732b134623e74ad
Morkinus <vygintasandr@gmail.com>
2016-03-12 12:02:22 +0200

    Chapter10 finished
    Scene sound effects control basics, playing sound effects on impacts;
    background music and fading of music

---
+++ UnityInAction/Chapter10/Basics/Assets/Scripts/UIController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	[SerializeField] private SettingsPopup popup;
+
+	// Use this for initialization
+	void Start () {
+		popup.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.M)) {
+			bool isShowing = popup.gameObject.activeSelf;
+			popup.gameObject.SetActive(!isShowing);
+
+			if(isShowing)
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+			else{
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+		}
+	}
+}
