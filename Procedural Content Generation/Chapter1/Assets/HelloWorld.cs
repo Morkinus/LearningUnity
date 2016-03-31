a1b6ae104e31243fdf52d78b4f6ca299f271fc31
Morkinus <vygintasandr@gmail.com>
2016-03-31 14:55:34 +0300

    Taking a leap into PCG world

---
+++ Procedural Content Generation/Chapter1/Assets/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+    public string[] hellos = new string[4] {
+        "Hello World", "Hola Mundo",
+        "Bonjour Le Monde", "Hallo Welt"
+    };
+
+    public Text textString;
+    
+    // Use this for initialization
+	void Start () {
+        Random.seed = (int)System.DateTime.Now.Ticks;
+        int randomIndex = Random.Range(0, hellos.Length);
+        textString.text = hellos[randomIndex];
+	}
+	
+	
+}
